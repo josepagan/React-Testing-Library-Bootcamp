2e98e66f3f69cb48378d5524b74cc73aa3bed6b2
Jose Pagan <jm.pagan@gmail.com>
2022-07-04 08:44:54 +0100

    events explained

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("renders App component", () => {
+    render(<App />);
+  });
+  it("all input elemts are empty", () => {
+    render(<App />);
+    const emailInputElement = screen.getByRole("textbox", { name: /email/i });
+    expect(emailInputElement.value).toBe("");
+      //needs regex anchors otherwise it would select "Confirm Password" too
+    const passwordInputElement = screen.getByLabelText(/^password$/i);
+    expect(passwordInputElement.value).toBe("");
+    const confirmPaswordInputElement = screen.getByLabelText(/confirm password/i);
+    expect(confirmPaswordInputElement.value).toBe("");
+  });
+});
+
+describe("testing input", () => {
+  it("is able to type email", () => {
+    render(<App />);
+    const emailInputElement = screen.getByRole("textbox", {
+      name: /email/i,
+    });
+      userEvent.type(emailInputElement, "jm.pagan@gmail.com")
+      expect(emailInputElement.value).toBe("jm.pagan@gmail.com")
+  });
 });
