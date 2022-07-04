f51ecffe440e281d092ca613dacd242c7025f637
Jose Pagan <jm.pagan@gmail.com>
2022-07-04 11:43:59 +0100

    deleted eslint and prettier for sanity sake

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
-import React from "react";
+import React, { useState }from "react";
 import "./App.css";
 
 function App() {
+  const handleChange = (e) => {
+    console.log("fuck eslint now with double quotes", e.target);
+  };
+    const [state, setState] = useState({email:"",password:"", confirmPassword:""})
   return (
     <div className="container my-5">
       <form>
         <div className="mb-3">
           <label htmlFor="email" className="form-label">
             Email address
-            <input type="email" id="email" name="email" className="form-control" />
+            <input type="email" id="email" name="email" className="form-control" onChange={handleChange} />
           </label>
         </div>
 
         <div className="mb-3">
           <label htmlFor="password" className="form-label">
             Password
-            <input type="password" id="password" name="password" className="form-control" />
+            <input type="password" id="password" name="password" className="form-control" onChange={handleChange} />
           </label>
         </div>
 
         <div className="mb-3">
           <label htmlFor="confirm-password" className="form-label">
             Confirm Password
-            <input type="password" id="confirm-password" name="confirm-password" className="form-control" />
+            <input
+              type="password"
+              id="confirm-password"
+              name="confirm-password"
+              className="form-control"
+              onChange={handleChange}
+            />
           </label>
         </div>
       </form>
